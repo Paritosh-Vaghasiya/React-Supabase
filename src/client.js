187226f8197deb3bc06918c6af63073d94e616d2
VIPCodeCoder <vaghasiya1923@students.d211.org>
2024-09-28 23:29:49 -0500

    fix

--- src/client.js
+++ src/client.js
@@ -1,5 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://hjudnxozhddpxofegjug.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhqdWRueG96aGRkcHhvZmVnanVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzAxMzE5NDcsImV4cCI6MTk4NTcwNzk0N30.YmIOoFGYxIBGfG1ICihRzHkfWXfLH2L49Tk_Sd9vqmc'
-export const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = "https://egzhuriimugvkjiauphl.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVnemh1cmlpbXVndmtqaWF1cGhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQwNzEzNjcsImV4cCI6MjAzOTY0NzM2N30.29e4s0hYCEB3e4m0GDB2WgSpEDbiJSSC4FOg5aU8ZOk";
+export const supabase = createClient(supabaseUrl, supabaseKey);
